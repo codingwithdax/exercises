467ac2ebdaaa0bcae62e7ff961d58d124dad5afb
codingwithdax <codingwithdax@gmail.com>
2022-02-07 09:11:31 -0500

    changes for problem 2

--- domjs_2/solution/domjs_2.js
+++ domjs_2/solution/domjs_2.js
@@ -1,23 +1,21 @@
 function problem2() {
-
     const problem2Div = document.getElementById("problem2_id");
 
     const olElement = document.createElement("ol");
-    olElement.setAttribute("style", "background-color: yellow; width: 50px")
+    olElement.setAttribute("style", "background-color: yellow; width: 50px");
     problem2Div.appendChild(olElement);
 
     const messageElement = document.getElementById("message_id");
     const countElement = document.getElementById("count_id");
 
+    const countValue = countElement.value;
     const message = messageElement.value;
-    const count = countElement.value;
 
-    for (let index = 0; index < count; index++) {
+    for (let index = 0; index < countValue; index++) {
         const liElement = document.createElement("li");
         olElement.appendChild(liElement);
 
         const appendedMessage = message + (index + 1);
-
         const textNode = document.createTextNode(appendedMessage);
         liElement.appendChild(textNode);
     }
